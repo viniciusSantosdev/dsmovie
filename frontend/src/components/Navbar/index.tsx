c5c43e089cdbc918450f743c9f403d32d5806c0b
Vinicius Santos <santos.viniciusmt@gmail.com>
2022-01-14 12:12:18 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,23 @@
+// reutilizando imagem como componente
+import { ReactComponent as GitHubIcon } from "assets/img/github.svg";
+import './styles.css';
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="dsmovie-nav-content">
+          <h1>DSMovie</h1>
+          <a href="https://github.com/viniciusSantosdev">
+            <div className="dsmovie-contact-container">
+              <GitHubIcon />
+              <p className="dsmovie-contact-link">/devsuperior</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
